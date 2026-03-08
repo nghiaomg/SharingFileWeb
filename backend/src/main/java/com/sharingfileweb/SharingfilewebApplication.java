52304b23f57ce8f6c7da49f9c67f2b433383d7a2
nghiaomg <trungnghia.260716@gmail.com>
2026-03-08 21:25:35 +0700

    Initial commit: Setup project structure with backend and frontend

---
+++ backend/src/main/java/com/sharingfileweb/SharingfilewebApplication.java
@@ -0,0 +1,13 @@
+package com.sharingfileweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingfilewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingfilewebApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sharingfileweb/SharingfilewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharingfileweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingfilewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
